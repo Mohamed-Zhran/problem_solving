3aab4ffbde6fd64f0796b2c32aeb8d4dfb0b5fa6
Mohamed Zhran <mohamedzhran100@gmail.com>
2024-12-14 21:29:03 +0200

    init

--- group_anagram/code.php
+++ group_anagram/code.php
@@ -3,7 +3,7 @@ $strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
 $result = [];
 
 foreach ($strs as $value) {
-    $key = count_chars($value);
+    $key = count_chars($value, 1);
     $result[json_encode($key)][] = $value;
 }
 print_r($result);
